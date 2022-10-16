49a46198be475da3f70ef2526f7a4f33642dbc1f
Rodolfo Nemes <tecnico.rodolfoo@gmail.com>
2022-10-16 14:44:34 -0300

    Saving a new Department

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -1,7 +1,5 @@
 package gui.util;
 
-
-
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.stage.Stage;
@@ -11,4 +9,13 @@ public class Utils {
 	public static Stage currentStage(ActionEvent event) {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 	}
+
+	public static Integer tryParseToInt(String str) {
+		try {
+			return Integer.parseInt(str);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
+
 }
